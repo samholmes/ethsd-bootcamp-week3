837805db382f8afa1ead6301b2b682d7802b7ab9
Samuel Holmes <sam@edge.app>
2023-01-27 18:36:25 -0800

    Move page body classes into the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,44 +7,46 @@ export function Home(props) {
   const { blocks } = props;
 
   return (
-    <div className="wrapper">
-      <section className="page-container">
-        <TopBan />
-        <LeftBar />
-        <div id="center">
-          <div id="searchDropdownGoesHereMobile"></div>
-          <div id="ccc" className="col">
-            <h3 className="ban">
-              <Link>
-                <span className="txt">
-                  blocks<sup className="c"></sup>
-                </span>
-              </Link>
-            </h3>
-            <div className="cats">
-              <ul id="ccc0">
-                {blocks.map((block) => {
-                  return (
-                    <li>
-                      <Link>
-                        <span className="txt">
-                          {block.number}
-                          <sup className="c"></sup>
-                        </span>
-                        <span className="txt">
-                          {block.hash}
-                          <sup className="c"></sup>
-                        </span>
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
+    <div className="homepage en bd-can-hover desktop w1024 cl-small-device cl-landscape">
+      <div className="wrapper">
+        <section className="page-container">
+          <TopBan />
+          <LeftBar />
+          <div id="center">
+            <div id="searchDropdownGoesHereMobile"></div>
+            <div id="ccc" className="col">
+              <h3 className="ban">
+                <Link>
+                  <span className="txt">
+                    blocks<sup className="c"></sup>
+                  </span>
+                </Link>
+              </h3>
+              <div className="cats">
+                <ul id="ccc0">
+                  {blocks.map((block) => {
+                    return (
+                      <li>
+                        <Link>
+                          <span className="txt">
+                            {block.number}
+                            <sup className="c"></sup>
+                          </span>
+                          <span className="txt">
+                            {block.hash}
+                            <sup className="c"></sup>
+                          </span>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-        <RightBar />
-      </section>
+          <RightBar />
+        </section>
+      </div>
     </div>
   );
 }
